0d67acba8ba224f0db44f4352c1e4457112560d0
amanmaurya104 <mouryaaman640@gmail.com>
2026-01-07 13:17:03 +0530

    Updates

---
+++ assets/clock-DeymK3Jy.js
@@ -0,0 +1,1 @@
+import{c}from"./index-Dv6g_AD1.js";const t=[["path",{d:"M 22 14 L 22 10",key:"nqc4tb"}],["rect",{x:"2",y:"6",width:"16",height:"12",rx:"2",key:"13zb55"}]],r=c("battery",t);const e=[["path",{d:"M12 6v6l4 2",key:"mmk7yg"}],["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}]],y=c("clock",e);export{r as B,y as C};
